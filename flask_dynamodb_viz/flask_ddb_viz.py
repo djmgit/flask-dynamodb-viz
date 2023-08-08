a2c64ec77f9c9b58935fd61df01863c013ad0e84
djmatlas <djmdeveloper060796@gmail.com>
2023-08-09 00:20:29 +0530

    Update flask ddb viz config

--- flask_dynamodb_viz/flask_ddb_viz.py
+++ flask_dynamodb_viz/flask_ddb_viz.py
@@ -10,12 +10,20 @@ class DDBTableInterface:
     def scan(*args, **kwargs):
         raise NotImplementedError("Method not implemented")
 
-class TableConfig(BaseModel):
-    table_name: str
-    table: DDBTableInterface
+class DDBResourceInterface:
+    class Table:
+        name: str
 
-class DDBTableFactory(BaseModel):
-    tables: List[TableConfig]
+    class Tables:
+        def all():
+            pass
+    
+    tables: Tables
+
+
+class FlaskDDBVizConfig(BaseModel):
+    ddb_resource: Any
+    allowed_tables: Optional[List[str]] = None
 
 def _big_scan(table: DDBTableInterface):
         scan_result = []
@@ -43,9 +51,16 @@ def _get_all(table: DDBTableInterface):
 def _get_table_from_factory(table_name: str, ddb_table_factory: DDBTableFactory) -> TableConfig:
     return list(filter(lambda table_config: table_config.table_name == table_name, ddb_table_factory.tables))[0]
 
-def _show_table_view(table_name: str, ddb_table_factory: DDBTableFactory):
-    table_config = _get_table_from_factory(table_name, ddb_table_factory)
-    ddb_table = table_config.table
+def _show_table_view(table_name: str, flask_ddb_viz_config: FlaskDDBVizConfig):
+    ddb_resource: DDBResourceInterface = flask_ddb_viz_config.ddb_resource
+    ddb_tables: List[DDBResourceInterface.Table] = ddb_resource.tables.all()
+    table_names = [table.name for table in ddb_tables]
+    if table_name not in table_names:
+        return {"error": "Table does not exist"}, 404
+    allowed_tables = flask_ddb_viz_config.allowed_tables
+    if allowed_tables and table_name not in allowed_tables:
+        return {"error": "Table cannot be shown"}, 403
+    ddb_table: DDBTableInterface = ddb_resource.Table(table_name)
     resultset = _get_all(ddb_table)
     return {"table_name": table_name, "items": resultset}, 200
    
@@ -57,7 +72,7 @@ class FlaskDDBViz:
             self.init_app(app)
     
     def init_app(self, app: Flask):
-        ddb_table_factory: DDBTableFactory = app.config["DDB_TABLE_FACTORY"]
-        app.add_url_rule("/ddb_table/<table_name>", view_func=lambda table_name: _show_table_view(table_name, ddb_table_factory), methods=["GET"])
+        flask_ddb_viz_config: FlaskDDBVizConfig = app.config["FLASK_DBB_VIZ_CONFIG"]
+        app.add_url_rule("/ddb_table/<table_name>", view_func=lambda table_name: _show_table_view(table_name, flask_ddb_viz_config), methods=["GET"])
     
 
