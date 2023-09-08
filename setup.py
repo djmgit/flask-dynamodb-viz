53778839bd945484c82941f26e70f1d7e41b258c
djmatlas <djmdeveloper060796@gmail.com>
2023-09-08 19:00:52 +0530

    Update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     license="",
     author="Deepjyoti Mondal",
     description="Dynamodb extension for Flask",
-    download_url="https://github.com/djmgit/flask-dynamodb-viz/archive/refs/tags/v0.0.2.tar.gz",
+    download_url="https://github.com/djmgit/flask-dynamodb-viz/archive/refs/tags/v0.0.3.tar.gz",
     long_description=__doc__,
     zip_safe=False,
     keywords = ["Flask", "Python", "Dynamodb", "AWS", "Extension", "Flask-Extension"],
@@ -30,5 +30,5 @@ setup(
         'Programming Language :: Python',
         'Topic :: System :: Monitoring',
     ],
-    version='0.0.2'
+    version='0.0.3'
 )
